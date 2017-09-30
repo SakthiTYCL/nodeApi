697cc124eb3f4bb87cb8aaa5b362795f9ed0c7e2
Sakthi <sakthi@tycl.org,in>
2017-09-30 23:26:46 +0530

    database url changed

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,8 @@
 //connection string for the database
 module.exports = {
     // ConnectionString: "mysql://adminwQ6MUP1:iTzEgJKEURiH@127.3.118.2:3306/nkanaapi",
-    // ConnectionString: "mysql://sql3197059:int8rSuzuX@sql3.freemysqlhosting.net:3306/sql3197059",
-    ConnectionString: "mysql://tyclorgi_nkana:nkana!@#$@72.52.80.134:3306/tyclorgi_nkanaapi",
+    ConnectionString: "mysql://sql3197059:int8rSuzuX@sql3.freemysqlhosting.net:3306/sql3197059",
+    // ConnectionString: "mysql://tyclorgi_nkana:nkana!@#$@tycl.org.in:3306/tyclorgi_nkanaapi",
     // ConnectionString: "mysql://adminwQ6MUP1:iTzEgJKEURiH@127.0.0.1:3306/nkanaapi",    
     settings: {
         define: {
