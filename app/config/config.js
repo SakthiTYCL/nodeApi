43e4447f5c4768042d535825ebd632b805c145af
SakthiTYCL <sakthi@tycl.org.in>
2017-10-24 20:17:38 +0530

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 //connection string for the database
 module.exports = {
     // ConnectionString: "mysql://adminwQ6MUP1:iTzEgJKEURiH@127.3.118.2:3306/nkanaapi",
-    ConnectionString: "mysql://sql12198292:dFB3K8l9Zz@sql12.freesqldatabase.com:3306/sql12198292",
+    ConnectionString: "mysql://sql12201101:ZTGk6hhYqq@sql12.freesqldatabase.com:3306/sql12201101",
     // ConnectionString: "mysql://tyclorgi_nkana:nkana!@#$@tycl.org.in:3306/tyclorgi_nkanaapi",
     // ConnectionString: "mysql://adminwQ6MUP1:iTzEgJKEURiH@127.0.0.1:3306/nkanaapi",    
     settings: {
